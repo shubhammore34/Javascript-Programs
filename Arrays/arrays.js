2b0ed2a94e0f955743fd4e52be8f2102a765ef7c
shubhammore34 <shubhammore8338@gmail.com>
2025-01-09 22:01:42 +0530

    second commit

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -78,3 +78,25 @@ const arr = [
 ] 
 console.log(arr) // arr containing different data types  
 
+/*
+Creating an array using split
+As we have seen in the earlier section, we can split a string at different positions, and we can change to an array. Let us see the examples below.
+*/
+let js = 'JavaScript'
+const charsInJavaScript = js.split('')
+
+console.log(charsInJavaScript) // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+
+console.log(companies) // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
+let txt =
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+const words = txt.split(' ')
+
+console.log(words)
+// the text has special characters think how you can just get only the words
+// ["I", "love", "teaching", "and", "empowering", "people.", "I", "teach", "HTML,", "CSS,", "JS,", "React,", "Python"]
+
+
