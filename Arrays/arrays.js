d00ea3a82f52561370aead982a2d06299314ad29
shubhammore34 <shubhammore8338@gmail.com>
2025-01-11 10:01:53 +0530

    updated

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -27,16 +27,16 @@ console.log(arr) //[]
 // Array with initial values. We use length property to find the length of an array.
 
 const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
-const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const $fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
 const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
 const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
-const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const webTechs2 = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
 const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
 
 // Print the array and its length
 
-console.log('Numbers:', numbers)
-console.log('Number of numbers:', numbers.length)
+console.log('$Numbers:', $numbers)
+console.log('Number of $numbers:', $numbers.length)
 
 console.log('Fruits:', fruits)
 console.log('Number of fruits:', fruits.length)
@@ -54,8 +54,8 @@ console.log('Countries:', countries)
 console.log('Number of countries:', countries.length)
 
 /*Outputs : 
-Numbers: [0, 3.14, 9.81, 37, 98.6, 100]
-Number of numbers: 6
+$Numbers: [0, 3.14, 9.81, 37, 98.6, 100]
+Number of $numbers: 6
 Fruits: ['banana', 'orange', 'mango', 'lemon']
 Number of fruits: 4
 Vegetables: ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
@@ -91,10 +91,16 @@ let js2 = 'JavaScript'
 const charsInJavaScript2 = js2.split()
 console.log(charsInJavaScript2) // [ 'JavaScript' ]
 
+let js3 = 'JavaScript'
+const charsInJavaScript3 = js3.split(',')
+console.log(charsInJavaScript3) // [ 'JavaScript' ]
+
+
 let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
 const companies = companiesString.split(',')
 
 console.log(companies) // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
+
 let txt =
   'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
 const words = txt.split(' ')
@@ -102,5 +108,124 @@ const words = txt.split(' ')
 console.log(words)
 // the text has special characters think how you can just get only the words
 // ["I", "love", "teaching", "and", "empowering", "people.", "I", "teach", "HTML,", "CSS,", "JS,", "React,", "Python"]
+
+
+/*Accessing array items using index
+We access each element in an array using their index. An array index starts from 0. The picture below clearly shows the index of each element in the array. */
+
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+let firstFruit = fruits[0] // we are accessing the first item using its index
+
+console.log(firstFruit) // banana
+
+secondFruit = fruits[1]
+console.log(secondFruit) // orange
+
+let lastFruit = fruits[3]
+console.log(lastFruit) // lemon
+// Last index can be calculated as follows
+
+let $lastIndex = fruits.length - 1
+lastFruit = fruits[lastIndex]
+
+console.log(lastFruit)  // lemon
+
+const $numbers = [0, 3.14, 9.81, 37, 98.6, 100]  // set of numbers
+
+console.log($numbers.length)  // => to know the size of the array, which is 6
+console.log($numbers)         // -> [0, 3.14, 9.81, 37, 98.6, 100]
+console.log($numbers[0])      //  -> 0
+console.log($numbers[5])      //  -> 100
+
+let lastIndex = $numbers.length - 1;
+console.log($numbers[lastIndex]) // -> 100
+
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+] // List of web technologies
+
+console.log(webTechs)        // all the array items
+console.log(webTechs.length) // => to know the size of the array, which is 7
+console.log(webTechs[0])     //  -> HTML
+console.log(webTechs[6])     //  -> MongoDB
+
+let lastIndex2 = webTechs.length - 1
+console.log(webTechs[lastIndex2]) // -> MongoDB
+
+const $countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+] // List of countries
+
+console.log($countries)      // -> all countries in array
+console.log($countries[0])   //  -> Albania
+console.log($countries[10])  //  -> Kenya
+
+let lastIndex3 = $countries.length - 1;
+console.log($countries[lastIndex3]) //  -> Kenya
+
+const shoppingCart = [
+  'Milk',
+  'Mango',
+  'Tomato',
+  'Potato',
+  'Avocado',
+  'Meat',
+  'Eggs',
+  'Sugar'
+] // List of food products
+
+console.log(shoppingCart) // -> all shoppingCart in array
+console.log(shoppingCart[0]) //  -> Milk
+console.log(shoppingCart[7]) //  -> Sugar
+
+let lastIndex4 = shoppingCart.length - 1;
+console.log(shoppingCart[lastIndex4]) //  -> Sugar
+
+/*Modifying array element
+An array is mutable(modifiable). Once an array is created, we can modify the contents of the array elements.
+*/
+
+const numbers3 = [1, 2, 3, 4, 5]
+numbers3[0] = 10      // changing 1 at index 0 to 10
+numbers3[1] = 20      // changing  2 at index 1 to 20
+
+console.log(numbers3) // [10, 20, 3, 4, 5]
+
+const countries3 = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+countries3[0] = 'Afghanistan'  // Replacing Albania by Afghanistan
+let lastIndex5 = countries3.length - 1
+countries3[lastIndex5] = 'Korea' // Replacing Kenya by Korea
+
+console.log(countries3) //  ["Afghanistan", "Bolivia", "Canada", "Denmark", "Ethiopia", "Finland", "Germany", "Hungary", "Ireland", "Japan", "Korea"]
 
 
