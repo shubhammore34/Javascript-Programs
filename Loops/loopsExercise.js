e356878c50c5059edb4a90eb02c37adb65b38725
shubhammore34 <shubhammore8338@gmail.com>
2025-01-17 21:49:47 +0530

    Create loopsExercise.js

---
+++ Loops/loopsExercise.js
@@ -0,0 +1,65 @@
+//Exercise level 1:
+//1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+for(let i=0; i<=10; i++) {
+    console.log(i)
+}
+
+let j=0;
+while(j<=10) {
+    console.log(j);
+    j++;
+}
+
+let k=0;
+do {
+    console.log(k);
+    k++;
+}
+while(k<=10)
+
+
+//2. Iterate 10 to 0 using for loop, do the same using while and do while loop
+for(let i=10; i>=0; i--) {
+    console.log(i)
+}
+
+let i=10;
+while(i>=0) {
+    console.log(i)
+    i--
+}
+
+let a=10;
+do {
+    console.log(a)
+    a--
+}
+while(a>=0)
+
+
+//3. Iterate 0 to n using for loop
+let n=2;
+for(let i=0; i<=n; i++)
+{
+    console.log(i);
+
+}
+
+/*4 Write a loop that makes the following pattern using console.log():
+
+#
+##
+###
+####
+#####
+######
+####### */
+
+console.log('#')
+console.log('##')
+console.log('###')
+console.log('####')
+console.log('#####')
+console.log('######')
+console.log('#######')
+
